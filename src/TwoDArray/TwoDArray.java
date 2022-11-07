6e1aeeb3f9e89d8fd72feb9405c442f8aca49fcb
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-11-07 19:20:28 +0600

    2d array

---
+++ src/TwoDArray/TwoDArray.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TwoDArray;
+
+/**
+ *
+ * @author nayeem
+ */
+public class TwoDArray {
+    public static void main(String[]args)
+    {
+    int[][] MyArray= new int[3][3];
+    MyArray[0][0]=1;
+    MyArray[0][1]=2;
+    MyArray[0][2]=3;
+    
+    MyArray[1][0]=5;
+    MyArray[1][1]=6;
+    MyArray[1][2]=7;
+    
+    MyArray[2][0]=8;
+    MyArray[2][1]=9;
+    MyArray[2][2]=5;
+    
+    System.out.println(MyArray[0][0]+" "+MyArray[0][1]+" "+MyArray[0][2] );
+    System.out.println(MyArray[1][0]+" "+MyArray[1][1]+" "+MyArray[1][2] );
+    System.out.println(MyArray[2][0]+" "+MyArray[2][1]+" "+MyArray[2][2] );
+    
+    
+    
+    
+    }
+    
+}
